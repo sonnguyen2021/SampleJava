7db4d4e5f4d9a6711f56bfa4f46953a66ebceb82
SonNguyen <son.nguyen2@halliburton.com>
2022-01-18 12:17:48 +0700

    comit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld
 {
     public static void main(String[] args){
         System.out.println("Hello World!!!!!");
-        System.out.println("Web Hook1!!!!!");
+        System.out.println("Web Hook2!!!!!");
     }
 }
